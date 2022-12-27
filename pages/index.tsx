5e42fa8b636e90e949b20c4d5ca9930a3ed63e72
ShoggyR79 <khaidu2002@gmail.com>
2022-12-27 02:32:26 -0600

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
@@ -69,7 +70,7 @@ export default function Home({
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
-            <img
+            <Image
               className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
               src="https://ps.w.org/back-to-the-top-button/assets/icon-256x256.png?rev=2283756"
               alt="scroll to top"
@@ -81,21 +82,21 @@ export default function Home({
   );
 }
 
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   const pageInfo: PageInfo = await fetchPageInfo();
-//   const socials: Social[] = await fetchSocials();
-//   const experiences: Experience[] = await fetchExperiences();
-//   const projects: Project[] = await fetchProjects();
-//   const skills: Skill[] = await fetchSkills();
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const socials: Social[] = await fetchSocials();
+  const experiences: Experience[] = await fetchExperiences();
+  const projects: Project[] = await fetchProjects();
+  const skills: Skill[] = await fetchSkills();
 
-//   return {
-//     props: {
-//       pageInfo,
-//       experiences,
-//       skills,
-//       projects,
-//       socials,
-//     },
-//     revalidate: 60,
-//   };
-// };
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 60,
+  };
+};
