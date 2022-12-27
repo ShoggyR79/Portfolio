a6607e428e863288e1e0b0ad0af30c654a4e6568
ShoggyR79 <khaidu2002@gmail.com>
2022-12-27 13:13:46 -0600

    tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jxs,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
