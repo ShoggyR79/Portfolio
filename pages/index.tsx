6b4a9ce67359c577d0bbec99111ae45bd1b335af
ShoggyR79 <khaidu2002@gmail.com>
2022-12-27 03:14:17 -0600

    hate it here

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -17,86 +17,87 @@ import { fetchSkills } from "../utils/fetchSkills";
 import { fetchSocials } from "../utils/fetchSocials";
 
 type Props = {
-  pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
-  socials: Social[];
+  // pageInfo: PageInfo;
+  // experiences: Experience[];
+  // skills: Skill[];
+  // projects: Project[];
+  // socials: Social[];
 };
 
 export default function Home({
-  pageInfo,
-  experiences,
-  skills,
-  socials,
-  projects,
+  // pageInfo,
+  // experiences,
+  // skills,
+  // socials,
+  // projects,
 }: Props) {
   return (
-    <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-track-gray-400/20 scrollbar-thin scrollbar-thumb-[#20AAB1]/80">
-      <Head>
-        <title>Du Duong</title>
-        <meta
-          name="description"
-          content="Portfolio website created by Du Duong"
-        />
-        <link rel="icon" href="/DDbyLK.png" />
-      </Head>
+    <h1>hello world!!!!</h1>
+    // <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-track-gray-400/20 scrollbar-thin scrollbar-thumb-[#20AAB1]/80">
+    //   <Head>
+    //     <title>Du Duong</title>
+    //     <meta
+    //       name="description"
+    //       content="Portfolio website created by Du Duong"
+    //     />
+    //     <link rel="icon" href="/DDbyLK.png" />
+    //   </Head>
 
-      <Header socials={socials} />
+    //   <Header socials={socials} />
 
-      <section id="hero">
-        <Hero pageInfo={pageInfo} />
-      </section>
+    //   <section id="hero">
+    //     <Hero pageInfo={pageInfo} />
+    //   </section>
 
-      <section id="about">
-        <About pageInfo={pageInfo} />
-      </section>
+    //   <section id="about">
+    //     <About pageInfo={pageInfo} />
+    //   </section>
 
-      <section id="experience">
-        <WorkExperiences experiences={experiences} />
-      </section>
+    //   <section id="experience">
+    //     <WorkExperiences experiences={experiences} />
+    //   </section>
 
-      <section id="skills">
-        <Skills skills={skills} />
-      </section>
-      {/* Projects */}
-      <section id="projects">
-        <Projects projects={projects} />
-      </section>
-      {/* Contact Me */}
-      <section id="contact">
-        <ContactMe />
-      </section>
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex items-center justify-center">
-            <Image
-              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-              src="https://ps.w.org/back-to-the-top-button/assets/icon-256x256.png?rev=2283756"
-              alt="scroll to top"
-            />
-          </div>
-        </footer>
-      </Link>
-    </div>
+    //   <section id="skills">
+    //     <Skills skills={skills} />
+    //   </section>
+    //   {/* Projects */}
+    //   <section id="projects">
+    //     <Projects projects={projects} />
+    //   </section>
+    //   {/* Contact Me */}
+    //   <section id="contact">
+    //     <ContactMe />
+    //   </section>
+    //   <Link href="#hero">
+    //     <footer className="sticky bottom-5 w-full cursor-pointer">
+    //       <div className="flex items-center justify-center">
+    //         <Image
+    //           className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+    //           src="https://ps.w.org/back-to-the-top-button/assets/icon-256x256.png?rev=2283756"
+    //           alt="scroll to top"
+    //         />
+    //       </div>
+    //     </footer>
+    //   </Link>
+    // </div>
   );
 }
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const socials: Social[] = await fetchSocials();
-  const experiences: Experience[] = await fetchExperiences();
-  const projects: Project[] = await fetchProjects();
-  const skills: Skill[] = await fetchSkills();
+// export const getStaticProps: GetServerSideProps<Props> = async () => {
+//   const pageInfo: PageInfo = await fetchPageInfo();
+//   const socials: Social[] = await fetchSocials();
+//   const experiences: Experience[] = await fetchExperiences();
+//   const projects: Project[] = await fetchProjects();
+//   const skills: Skill[] = await fetchSkills();
 
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
-    },
-    revalidate: 30,
-  };
-};
+//   return {
+//     props: {
+//       pageInfo,
+//       experiences,
+//       skills,
+//       projects,
+//       socials,
+//     },
+//     revalidate: 30,
+//   };
+// };
